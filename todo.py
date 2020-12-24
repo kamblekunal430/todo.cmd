efc062cbf46f1cde2ae549134bdd03bd4b637c9c
kamblekunal430 <kamblekunal430@gmail.com>
2020-12-24 16:22:03 +0530

    added add feature to add new item to the list

--- todo.py
+++ todo.py
@@ -19,14 +19,23 @@ def ls():
         f = open("todo.txt",'r')
         todo_list = f.readlines()
         task_count = len(todo_list)
+        print(todo_list)
         for task in todo_list[::-1]:
-            print("[{}] {}".format(task_count,task))
+            print("[{}] {}".format(task_count,task[:-1]))
             task_count-=1
 
     finally:
         f.close()
 
-
+# Function to add new todo item
+def add(item):
+    try:
+        # Opening the file in append mode to add the item
+        f = open("todo.txt","a")
+        f.write(str(item)+"\n")
+        
+    finally:
+        f.close()
 
 
 # Calculating the lenght of arguments recieved
@@ -37,5 +46,11 @@ if (arg_len == 1):
     help()
 
 # Display the list of pending todos
-if sys.argv[1] == 'ls': 
+if arg_len == 2 and sys.argv[1] == 'ls': 
     ls()
+
+
+# Adding new task to the todo list
+if arg_len > 2 and sys.argv[1] == "add":
+    add(sys.argv[2])
+    print('Added todo: "{}"'.format(sys.argv[2]))
