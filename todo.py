9fd4e84e486689322c38deaae41613d2d4da053c
kamblekunal430 <kamblekunal430@gmail.com>
2020-12-25 18:51:33 +0530

    updated help menu

--- todo.py
+++ todo.py
@@ -3,13 +3,13 @@ import sys
 
 # help function to show what can the TODO do.
 def help():
-    print("Usage:-")
-    print('$ ./todo add "todo item" # Add a new todo')
-    print('$ ./todo ls              # Show remaining todos')
-    print('$ ./todo del NUMBER      # Delete a todo')
-    print('$ ./todo done NUMBER     # Complete a todo')
-    print('$ ./todo help            # Show usage')
-    print('$ ./todo report          # Statistics')
+    print("Usage :-")
+    print('$ ./todo add "todo item"  # Add a new todo')
+    print('$ ./todo ls               # Show remaining todos')
+    print('$ ./todo del NUMBER       # Delete a todo')
+    print('$ ./todo done NUMBER      # Complete a todo')
+    print('$ ./todo help             # Show usage')
+    print('$ ./todo report           # Statistics')
 
 
 # Function to list all pending todos
@@ -54,3 +54,12 @@ if arg_len == 2 and sys.argv[1] == 'ls':
 if arg_len > 2 and sys.argv[1] == "add":
     add(sys.argv[2])
     print('Added todo: "{}"'.format(sys.argv[2]))
+
+
+print("Usage :-")
+print('$ ./todo add "todo item"  # Add a new todo')
+print('$ ./todo ls               # Show remaining todos')
+print('$ ./todo del NUMBER       # Delete a todo')
+print('$ ./todo done NUMBER      # Complete a todo')
+print('$ ./todo help             # Show usage')
+print('$ ./todo report           # Statistics')
